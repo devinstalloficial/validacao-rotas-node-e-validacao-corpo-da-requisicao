aa216473f912002ee0580464b6cebd2572f22430
claudio-code <suporte2424@outlook.com>
2020-04-18 21:49:23 -0300

    passando validacao do token para execultar em todas as rotas desse arquivo

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-
+const userAlth = require('../middlewares/userAlth');
 const UserController = require('../controllers/UserController');
 
 const userRoutes = new Router();
+userRoutes.use(userAlth);
 
 userRoutes.get('/user', UserController.index);
 
