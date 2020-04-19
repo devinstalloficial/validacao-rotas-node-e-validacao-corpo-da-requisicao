c323e38076d08b09fb11b844eb77d5183fc2cd4e
claudio-code <suporte2424@outlook.com>
2020-04-18 21:49:14 -0300

    passando validacao do token para execultar em todas as rotas desse arquivo

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
-
+const adminAlth = require('../middlewares/adminAlth');
 const AdminController = require('../controllers/AdminController');
 
 const adminRoutes = new Router();
 
+adminRoutes.use(adminAlth);
 adminRoutes.get('/admin', AdminController.index);
 
 module.exports = adminRoutes;
